2368b75809ee3f774a5f7cf9c163787a3361e9c0
Robb C. Bennett <Visual23@Robbs-Mac-Pro.local>
2015-01-05 14:32:29 -0500

    Tried building the ballon struct again

---
+++ 99RedBallons/Ballon.swift
@@ -0,0 +1,16 @@
+//
+//  Ballon.swift
+//  99RedBallons
+//
+//  Created by Robb C. Bennett on 1/5/15.
+//  Copyright (c) 2015 Visual23. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Ballon {
+    
+    var ballonNumber = 0;
+    var ballonImage = UIImage(named:"")
+}
